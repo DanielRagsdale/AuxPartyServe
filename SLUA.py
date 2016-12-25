575ee9f15930ce87db4b59fa12dc6953141d43c6
Daniel Ragsdale <daniel@studiodan.io>
2016-12-25 15:07:33 -0800

    Added a utility to look up songs and send a post request to add them to the database

---
+++ SLUA.py
@@ -0,0 +1,21 @@
+import sys
+import requests
+
+r = requests.get('https://itunes.apple.com/search?term=' + sys.argv[1] + '&entity=song&limit=1')
+
+print('https://itunes.apple.com/search?term=' + sys.argv[1] + '&entity=song&limit=1')
+
+results = r.json().get('results')
+apple_id = -1
+
+
+if len(results) > 0:
+    apple_id = results[0].get(u'trackId')
+
+    payload = {'apple_id': str(apple_id)}
+
+    p = requests.post('http://localhost:8000/api/00000', data=payload)
+
+    print(p.text)
+
+# print(r.json())
