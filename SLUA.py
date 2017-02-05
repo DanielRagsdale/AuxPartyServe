fc09103754e303845ec9ef94aa90fbf4ac735a1f
Daniel Ragsdale <daniel@studiodan.io>
2017-02-04 18:12:58 -0800

    Updated Search/LookUp/Add

--- SLUA.py
+++ SLUA.py
@@ -12,9 +12,9 @@ apple_id = -1
 if len(results) > 0:
     apple_id = results[0].get(u'trackId')
 
-    payload = {'apple_id': str(apple_id)}
+    payload = {'play_id': str(apple_id), 'service_name':'apple_music'}
 
-    p = requests.post('http://auxparty.com/api/client/request/00000', data=payload)
+    p = requests.post('http://auxparty.com/api/client/request/AAAAA', data=payload)
 
     print(p.text)
 
